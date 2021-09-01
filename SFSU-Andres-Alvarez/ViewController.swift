ef77c938b4ec98a7d98a43163e279e126cacda04
Andres Ariel Alvarez <andres@dreisigma.com>
2021-08-31 22:29:55 -0700

    Initial Commit

---
+++ SFSU-Andres-Alvarez/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SFSU-Andres-Alvarez
+//
+//  Created by Andres Ariel Alvarez on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SFSU-Andres-AlvarezTests/SFSU_Andres_AlvarezTests.swift
@@ -0,0 +1,33 @@
+//
+//  SFSU_Andres_AlvarezTests.swift
+//  SFSU-Andres-AlvarezTests
+//
+//  Created by Andres Ariel Alvarez on 8/31/21.
+//
+
+import XCTest
+@testable import SFSU_Andres_Alvarez
+
+class SFSU_Andres_AlvarezTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
